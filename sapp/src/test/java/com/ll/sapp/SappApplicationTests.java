295ad7c53d0e8d30a493532f2406c6e478285113
seyeong <seyoung4503@gmail.com>
2024-07-29 14:58:24 +0900

    test : Sapp Test 로컬 데이터베이스 데이터 추가

--- sapp/src/test/java/com/ll/sapp/SappApplicationTests.java
+++ sapp/src/test/java/com/ll/sapp/SappApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ll.sapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SappApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
 	@Test
+	void testJPA() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해 알고 싶어요.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
+
+
+	}
 	void contextLoads() {
+
 	}
 
 }
