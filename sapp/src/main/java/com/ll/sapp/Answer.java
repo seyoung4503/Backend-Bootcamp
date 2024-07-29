96fe861e655d9ac3192fbec1aa9d0daa7b5d2c5f
seyeong <seyoung4503@gmail.com>
2024-07-29 14:36:12 +0900

    feat : Question, Answer DB table

---
+++ sapp/src/main/java/com/ll/sapp/Answer.java
@@ -0,0 +1,25 @@
+package com.ll.sapp;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "Text")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+}
