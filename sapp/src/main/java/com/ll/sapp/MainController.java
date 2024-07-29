924541c481b9417816956793dfb0054cfa526365
seyeong <seyoung4503@gmail.com>
2024-07-29 13:54:40 +0900

    feat : MainController

---
+++ sapp/src/main/java/com/ll/sapp/MainController.java
@@ -0,0 +1,15 @@
+package com.ll.sapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        System.out.println("index");
+        return "안녕하세요";
+    }
+}
