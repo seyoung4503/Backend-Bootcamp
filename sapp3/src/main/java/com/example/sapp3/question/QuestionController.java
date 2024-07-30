39cbd2845be0acfb230df44e244a1cdef6cfe850
seyeong <seyoung4503@gmail.com>
2024-07-30 11:51:07 +0900

    docs : QuestionService getList를 통해 컨트롤러에 부여

--- sapp3/src/main/java/com/example/sapp3/question/QuestionController.java
+++ sapp3/src/main/java/com/example/sapp3/question/QuestionController.java
@@ -10,14 +10,19 @@ import java.util.List;
 @RequiredArgsConstructor // 롬복이 생성, final이 붙은 생성자를 자동으로 만들어준다. questionRepository
 @Controller
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+//    private final QuestionRepository questionRepository;
+
+    private final QuestionService questionService;
     @GetMapping("/question/list")
 //    @ResponseBody
     public String list(Model model) {
-        // Model은 자바 클래스와 템플릿 간의 연결고리이다.
-        List<Question> questionList = this.questionRepository.findAll();
-        model.addAttribute("questionList", questionList);
+//        // Model은 자바 클래스와 템플릿 간의 연결고리이다.
+//        List<Question> questionList = this.questionRepository.findAll();
+//        model.addAttribute("questionList", questionList);
+//
 
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
