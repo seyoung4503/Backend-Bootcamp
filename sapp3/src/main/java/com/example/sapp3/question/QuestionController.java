c7201ea6c383e4447b46bff3decd7f6fcd15acba
seyeong <seyoung4503@gmail.com>
2024-07-30 13:36:55 +0900

    refactor : QuestionController URL 프리픽스 추가

--- sapp3/src/main/java/com/example/sapp3/question/QuestionController.java
+++ sapp3/src/main/java/com/example/sapp3/question/QuestionController.java
@@ -5,16 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor // 롬복이 생성, final이 붙은 생성자를 자동으로 만들어준다. questionRepository
 @Controller
 public class QuestionController {
 //    private final QuestionRepository questionRepository;
 
     private final QuestionService questionService;
-    @GetMapping("/question/list")
+    @GetMapping("/list")
 //    @ResponseBody
     public String list(Model model) {
 //        // Model은 자바 클래스와 템플릿 간의 연결고리이다.
@@ -27,7 +29,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
