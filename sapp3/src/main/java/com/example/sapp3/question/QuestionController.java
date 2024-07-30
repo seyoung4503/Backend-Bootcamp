25b5d1a9bafaea9264a112d7c35a2a5b8cfd6fb4
seyeong <seyoung4503@gmail.com>
2024-07-30 13:24:53 +0900

    feat : QuestionController detail 추가 docs : question_detail.html 추가

--- sapp3/src/main/java/com/example/sapp3/question/QuestionController.java
+++ sapp3/src/main/java/com/example/sapp3/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,5 +27,9 @@ public class QuestionController {
         return "question_list";
     }
 
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
+    }
 
 }
