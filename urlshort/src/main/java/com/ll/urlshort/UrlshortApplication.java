da5cea4ddac4f9ce63beafc09ebae8374da8e1f3
seyeong <seyoung4503@gmail.com>
2024-08-01 13:45:50 +0900

    initial commit

---
+++ urlshort/src/main/java/com/ll/urlshort/UrlshortApplication.java
@@ -0,0 +1,13 @@
+package com.ll.urlshort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortApplication.class, args);
+	}
+
+}
---
+++ urlshort/src/test/java/com/ll/urlshort/UrlshortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.urlshort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
